648be10d533370faa0b7a7f54afa9f4cdb0ce44c
guchi-bottom <yuri.1125.dev@gmail.com>
2023-03-04 14:15:02 +0900

    ソースコード追加, README.md修正

---
+++ sample/src/views/NekochanKawaiine.vue
@@ -0,0 +1,30 @@
+<template>
+            <v-card
+            elevation="5"
+            >
+                <v-card-title>
+                    ﾈｺﾁｬﾝを崇めよ
+                </v-card-title>
+                <v-card-subtitle>
+                    nekochan is GOD
+                </v-card-subtitle>
+                <v-card-text>
+                    猫は全てかわいい<br />
+                    彼らは神の使いである
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn>
+                        ネコを吸う
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+</template>
+
+<script>
+    export default {
+    name: 'NekochanKawaiine',
+
+    data: () => ({
+    }),
+}
+</script>

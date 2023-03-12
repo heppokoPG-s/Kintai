beb3ad88749934bfc01c8561ce001edf9a1a2654
Akira-cun <hinokino1039@gmail.com>
2023-03-13 00:06:38 +0900

    画面遷移お試し作成
    Laravel8.0からControllerの連携の書き方が変わったらしく注意が必要

--- vue-laravel-spa/routes/web.php
+++ vue-laravel-spa/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\postTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/inputForm', [postTestController::class,'inputForm']); // 入力フォーム画面(inputForm)のURL割当、起動コントローラ・関数指定
+Route::post('/formPost', [postTestController::class,'formPost']);//結果画面(resultPage)のURL割当、起動コントローラ・関数指定
